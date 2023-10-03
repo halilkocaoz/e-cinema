743bd9d2a7e1e7abba816800efcb90fb08e53321
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-10-04 00:53:23 +0300

    update record

--- src/ECinema.MovieHouse.WebAPI/Commands/MovieHouses/Create/CreateMovieHouseCommand.cs
+++ src/ECinema.MovieHouse.WebAPI/Commands/MovieHouses/Create/CreateMovieHouseCommand.cs
@@ -2,4 +2,4 @@ using MediatR;
 
 namespace ECinema.MovieHouse.WebAPI.Commands.MovieHouses.Create;
 
-public record CreateMovieHouseCommand(string Name, List<string> MovieGenres) : IRequest<bool>;
+public sealed record CreateMovieHouseCommand(string Name, List<string> MovieGenres) : IRequest<bool>;
