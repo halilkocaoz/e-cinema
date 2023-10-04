d9aaf3797152abded72821b3d1cadaa445cf0f2e
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-10-04 20:28:41 +0300

    update base entity class to make a method as protected

--- src/ECinema.Common/Infrastructure/Models/Entity.cs
+++ src/ECinema.Common/Infrastructure/Models/Entity.cs
@@ -22,7 +22,7 @@ public abstract class Entity
     [JsonIgnore]
     public IReadOnlyCollection<INotification>? DomainEvents => _domainEvents?.AsReadOnly();
     
-    public void AddDomainEvent(INotification eventItem)
+    protected void AddDomainEvent(INotification eventItem)
     {
         _domainEvents ??= new List<INotification>();
         _domainEvents.Add(eventItem);
