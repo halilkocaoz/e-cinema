ab3e2cdca85de4b05284debc06998dc9ab791fab
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-10-04 17:34:12 +0300

    update mongodb repository, remove authentication

--- src/ECinema.Common/Infrastructure/MongoRepository.cs
+++ src/ECinema.Common/Infrastructure/MongoRepository.cs
@@ -9,32 +9,14 @@ namespace ECinema.Common.Infrastructure;
 public abstract class MongoRepository<T> : IRepository<T> where T : Entity
 {
     private readonly IMongoCollection<T> _collection;
-
     [Obsolete("Obsolete")]
-    private static MongoClientSettings GetMongoClientSettings(MongoDbSettings mongoDbSettings)
-    {
-        var internalIdentity = new MongoInternalIdentity(mongoDbSettings.Database, mongoDbSettings.UserName);
-        var passwordEvidence = new PasswordEvidence(mongoDbSettings.Password);
-        var mongoCredential = new MongoCredential(mongoDbSettings.AuthMechanism, internalIdentity, passwordEvidence);
-        var mongoClientSettings = new MongoClientSettings
-        {
-            Credential = mongoCredential,
-            ReadPreference = ReadPreference.SecondaryPreferred,
-            WriteConcern = WriteConcern.Acknowledged,
-            GuidRepresentation = GuidRepresentation.Standard
-        };
-        return mongoClientSettings;
-    }
-    
-    [Obsolete("Obsolete")]
-    private MongoRepository(IOptions<MongoDbSettings> options)
+    public MongoRepository(IOptions<MongoDbSettings> options)
     {
         var mongoDbSettings = options.Value ?? throw new ArgumentNullException(nameof(options));
-        var mongoClientSettings = GetMongoClientSettings(mongoDbSettings);
 
-        var client = new MongoClient(mongoClientSettings);
+        var client = new MongoClient(mongoDbSettings.ConnectionString);
         var db = client.GetDatabase(mongoDbSettings.Database);
-        
+
         _collection = db.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
     }
 
@@ -57,7 +39,7 @@ public abstract class MongoRepository<T> : IRepository<T> where T : Entity
 
     public virtual async Task<T> AddAsync(T entity)
     {
-        var options = new InsertOneOptions {BypassDocumentValidation = false};
+        var options = new InsertOneOptions { BypassDocumentValidation = false };
         await _collection.InsertOneAsync(entity, options);
         return entity;
     }
@@ -77,13 +59,7 @@ public class MongoDbSettings
 {
     public string? ConnectionString { get; set; }
     public string? Database { get; set; }
-    public string? UserName { get; set; }
-    public string? Password { get; set; }
-    public string? AuthMechanism { get; set; }
 
     public const string ConnectionStringValueName = nameof(ConnectionString);
     public const string DatabaseValueName = nameof(Database);
-    public const string UserNameValueName = nameof(UserName);
-    public const string PasswordValueName = nameof(Password);
-    public const string AuthMechanismValueName = nameof(AuthMechanism);
 }
