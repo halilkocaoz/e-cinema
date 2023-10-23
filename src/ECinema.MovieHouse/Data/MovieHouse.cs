e48d27df46bb457949a7e89206c7710aa8fa1472
halilkocaoz <halil.i.kocaoz@gmail.com>
2023-10-23 23:44:45 +0300

    update movie house entity property setting ways.

--- src/ECinema.MovieHouse/Data/MovieHouse.cs
+++ src/ECinema.MovieHouse/Data/MovieHouse.cs
@@ -5,7 +5,7 @@ namespace ECinema.MovieHouse.Data;
 public class MovieHouse(string name, List<string> interestedGenres, List<string> willBeInformedEmails)
     : MongoEntity
 {
-    public string Name { get; set; } = name;
-    public List<string> InterestedGenres { get; set; } = interestedGenres;
-    public List<string> WillBeInformedEmails { get; set; } = willBeInformedEmails;
+    public string Name { get; private set; } = name;
+    public List<string> InterestedGenres { get; private set; } = interestedGenres;
+    public List<string> WillBeInformedEmails { get; private set; } = willBeInformedEmails;
 }
